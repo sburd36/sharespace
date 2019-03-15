b7bd3fd6cb3da443b0f81818861b1a22e007e53a
my828 <minyang828@gmail.com>
2019-03-15 01:12:01 -0700

    commit

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,11 +1,18 @@
 import React from "react";
+import  CustomizedInputs  from "./material/Input"
+import Button from '@material-ui/core/Button';
 
 export default class Login extends React.Component {
     render() {
         return (
             <div>
-                
+                <CustomizedInputs>
+                </CustomizedInputs>
+                <CustomizedInputs>
+                </CustomizedInputs>
+                <button type="button" class="btn btn-primary">LOG IN</button>
             </div>
         )
     }
 }
+
