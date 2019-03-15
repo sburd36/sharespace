b72a76507c239af2bad690473cd33e6d5b20930e
sburd36 <sburd36@uw.edu>
2019-03-14 22:53:43 -0700

    finished styling sign up

--- client/src/Stay.js
+++ client/src/Stay.js
@@ -5,6 +5,11 @@ import Animated from 'react-select/lib/animated'
 import chaya from "./img/apichaya.png"
 import info from './img/iSchool.png';
 import mary from './img/marysplace.png';
+import physical from './img/physical-safety.jpg'
+import mental from './img/mental-health.jpg'
+import children from './img/children.jpg'
+import finances from './img/finances.jpg'
+
 import {BrowserRouter as Router, Route, Link} from "react-router-dom"
 
 export default class Stay extends React.Component {
@@ -42,18 +47,22 @@ export default class Stay extends React.Component {
         console.log(this.state.next)
 
         return ( 
-            <div>
+            <div id="place-to-stay">
                 {   !this.state.next && 
-                    <div class="d-flex justify-content-around p-5" style={{padding: "20px"}}>
+                    <>
+                    <Link to='/signup'>
+                        <h4 class="back-button">BACK</h4>
+                    </Link>
+                    <div class="d-flex justify-content-around" style={{padding: "20px"}}>
                     <div class="card" style={{width: "30rem"}}>
                         <img class="card-img-top" src={connect}></img>
                         <div class="card-body">
-                            <h5>I need a place to stay</h5>
+                            <h4 class="font-light">I need a place to stay</h4>
                         </div>
                     </div>
                     <div style={{width: window.screen.availWidth / 2}}>
-                        <div class="d-flex align-items-center p-5">
-                            <h5>Preferred neighborhood</h5> 
+                        <div class="d-flex align-items-center mb-5">
+                            <p class="mb-0">Preferred neighborhood</p> 
                             <div style={{paddingLeft: "20px", width: "500px"}}>                               
                                 <Select
                                     closeMenuOnSelect={false}
@@ -63,76 +72,87 @@ export default class Stay extends React.Component {
                                 />
                             </div>
                         </div>
-                        <h4>I'm concerned about...</h4>
-                        <div class="d-flex justify-content-center align-items-end p-3" >
+                        <h4 class="font-light">I'm concerned about...</h4>
+                        <div class="row d-flex justify-content-center align-items-end p-3 concerns" >
                             <div id="type1" class="card m-3" 
                             style={{width: "10rem", cursor: "pointer"}}
                             onClick={this.handleClick}>
-                                <img class="card-img-top" src={connect}></img>
-                                <div class="card-body" style={{fontSize: "20px"}}>
-                                    Type
+                                <img class="card-img-top" src={physical}></img>
+                                <div class="card-body">
+                                    Physical Safety
                                 </div>
                             </div>
                             <div id="type2" class="card m-3" style={{width: "10rem", cursor: "pointer"}}
                                 onClick={this.handleClick}>
-                                <img class="card-img-top" src={connect}></img>
-                                <div class="card-body" style={{fontSize: "20px"}}>
-                                    Type
+                                <img class="card-img-top" src={mental}></img>
+                                <div class="card-body">
+                                    Mental Health
                                 </div>
                             </div>
                             <div id="type3" class="card m-3" style={{width: "10rem", cursor: "pointer"}}
                                 onClick={this.handleClick}>
-                                <img class="card-img-top" src={connect}></img>
-                                <div class="card-body" style={{fontSize: "20px"}}>
-                                    Type
+                                <img class="card-img-top" src={finances}></img>
+                                <div class="card-body">
+                                    Finances
                                 </div>
                             </div>
                             <div id="type4" class="card m-3" style={{width: "10rem", cursor: "pointer"}}
                                 onClick={this.handleClick}>
-                                <img class="card-img-top" src={connect}></img>
-                                <div class="card-body" style={{fontSize: "20px"}}>
-                                    Type
+                                <img class="card-img-top" src={children}></img>
+                                <div class="card-body">
+                                    Children
+                                </div>
+                            </div>
+                            <div id="type5" class="card m-3" style={{width: "10rem", cursor: "pointer"}}
+                                onClick={this.handleClick}>
+                                <img class="card-img-top" src={children}></img>
+                                <div class="card-body">
+                                    Lorem Ipsum
                                 </div>
                             </div>                                 
                         </div> 
                         {/* <Link to="/organization" onClick={this.handleNext}>
                             Next
                         </Link> */}
-                        <button type="button" class="btn btn-primary" onClick={this.handleNext} >
-                            Next
+                        <button type="button" class="btn btn-teal-fill" onClick={this.handleNext} >
+                            NEXT
                         </button> 
                     </div>
                 </div>  
+                </>
             }               
             {
                 this.state.next && 
-                <div class="m-5">
-                    <h2 class="m-5">Here are your neighborhood organizations</h2>
+                <div class="m-5 orgs">
+                    <h3 class="m-5 font-light">Here are your neighborhood organizations</h3>
                     <div class="d-flex justify-content-around">
                         <div class="card" style={{width: "15rem"}}>
                             <img class="card-img-top mt-4" src={chaya}
                             style={{width: '10rem', position: "relative", left: "3rem"}}/>
                             <div class="card-body">
-                                <h4>Advocate Name</h4>
-                                organization@org.com   
-                                +1(206) 123-4567
+                                {/* <h4>API Chaya</h4> */}
+                                <a href="www.apichaya.org">apichaya.org</a>
+                                <p><a href="mailto:info@apichaya.org">info@apichaya.org</a></p>
+                                <p>+1 (206)467-9976</p>
                             </div>
                         </div>
                         <div class="card" style={{width: "15rem"}}>
                             <img class="card-img-top mt-4" src={info} 
                             style={{width: '8rem', position: "relative", left: "3rem"}}/>
                             <div class="card-body">
-                                <h4>Advocate Name</h4>
-                                organization@org.com
-                                +1(206) 123-4567
+                                {/* <h4>Information School</h4> */}
+                                <a href="www.apichaya.org">Website</a>
+                                <p><a href="mailto:info@apichaya.org">info@apichaya.org</a></p>
+                                <p>+1 (206)467-9976</p>
                             </div>
                         </div>
                         <div class="card" style={{width: "15rem"}}>
                             <img class="card-img-top mb-4 mt-5 p-2" src={mary}/>
                             <div class="card-body">
-                                <h4>Advocate Name</h4>
-                                organization@org.com
-                                +1(206) 123-4567
+                                {/* <h4>Mary's Place</h4> */}
+                                <a href="www.marysplaceseattle.org">marysplaceseattle.org</a>
+                                <p><a href="mailto:info@marysplaceseattle.org">info@marysplaceseattle.org</a></p>
+                                <p>+1 (206)621-8474</p>
                             </div>
                         </div>
                     </div>
