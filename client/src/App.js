4f1b3628e0a9ef022302b3d7ac0327ab1395d840
my828 <minyang828@gmail.com>
2019-04-28 19:03:30 -0700

    advocate view

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './style/index.css';
 import Nav from './Nav'
-import SignUp from './Situation'
+import Situation from './Situation'
 import Stay from './Stay'
 import Landing from './landing'
 import Footer from './Footer'
 import AboutUs from './AboutUs'
 import OurTool from './OurTool'
 import Advocate from './Advocate'
-import SignIn from './SignIn'
 import Calendar from './Calendar'
+import Dash from './Dash'
+
 
 import {HashRouter as Router, Switch, Redirect, Route} from "react-router-dom";
 
@@ -22,13 +23,13 @@ class App extends Component {
             <Nav />
               <Switch>
                   <Route exact path="/" component={Landing} />
-                  <Route path="/signup" component={SignUp} />
+                  <Route path="/situation" component={Situation} />
                   <Route path="/stay" component={Stay} />
                   <Route path="/aboutus" component={AboutUs} />
                   <Route path="/ourtool" component={OurTool} />
                   <Route path="/advocate" component={Advocate} />
-                  <Route path="/signin" component={SignIn} />
                   <Route path="/calendar" component={Calendar} />
+                  <Route path="/dash" component={Dash} />
               </Switch>
           </div>    
         </Router>
@@ -39,13 +40,3 @@ class App extends Component {
 }
 
 export default App;
-// <img src={logo} className="App-logo" alt="logo" />
-
-// <a
-//   className="App-link"
-//   href="https://reactjs.org"
-//   target="_blank"
-//   rel="noopener noreferrer"
-// >
-//   Learn React
-// </a>
