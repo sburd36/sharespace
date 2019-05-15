0cace7955b3df12cc3a9e96fc75f240b53444e63
my828 <minyang828@gmail.com>
2019-05-15 12:02:20 -0700

    commit

--- client/src/Advocate/CurrentBookings.js
+++ client/src/Advocate/CurrentBookings.js
@@ -72,7 +72,7 @@ export default withStyles(styles)(class extends React.Component {
         }
     }
 
-    handlCardClick = () => {
+    handleCardClick = () => {
         this.setState({
             open: !this.state.open
         })
